b3ade161302deb6bc3af2fe060c993bd2d6a31d7
Martin Burga <martin.burga@udla.edu.ec>
2024-01-27 22:53:19 -0500

    III): Se ha anadido los valores a las respectivas acciones de accion a realizar conforme al dispositivo. Se ha añadido la posibilidad de agregar un usuario y que se lo imprima al final con el respectivo valor de su cuenta.

--- basiccode.c
+++ basiccode.c
@@ -4,6 +4,7 @@ int labor;
 int dispositivo;
 int mantenimiento;
 int cuenta = 0;
+char cliente[50];
 
 int main()
 {
@@ -19,6 +20,8 @@ int main()
 
         if (opcion == 1)
         {
+            printf("Escribe el nombre del cliente.\n");
+            scanf("%s", cliente);
             printf("Selecciona el labor a realizar.\n");
             printf("1. Mantenimiento.\n");
             printf("2. Limpieza.\n");
@@ -36,6 +39,14 @@ int main()
                     printf("1. Computadora.\n");
                     printf("2. Celular.\n");
                     scanf("%d", &dispositivo);
+                    if (dispositivo == 1)
+                    {
+                        cuenta += 40;
+                    }
+                    else if (dispositivo == 2)
+                    {
+                        cuenta += 35;
+                    }
                 }
                 else if (mantenimiento == 2)
                 {
@@ -43,6 +54,14 @@ int main()
                     printf("1. Computadora.\n");
                     printf("2. Celular.\n");
                     scanf("%d", &dispositivo);
+                    if (dispositivo == 1)
+                    {
+                        cuenta += 50;
+                    }
+                    else if (dispositivo == 2)
+                    {
+                        cuenta += 45;
+                    }
                 }
                 else if (mantenimiento == 3)
                 {
@@ -50,6 +69,15 @@ int main()
                     printf("1. Computadora.\n");
                     printf("2. Celular.\n");
                     scanf("%d", &dispositivo);
+
+                    if (dispositivo == 1)
+                    {
+                        cuenta += 8;
+                    }
+                    else if (dispositivo == 2)
+                    {
+                        cuenta += 10;
+                    }
                 }
             }
             else if (labor == 2)
@@ -58,14 +86,31 @@ int main()
                 printf("1. Computadora.\n");
                 printf("2. Celular.\n");
                 scanf("%d", &dispositivo);
-            }
+                if (dispositivo == 1)
+                {
+                    cuenta += 30;
+                }
+                else if (dispositivo == 2)
+                {
+                    cuenta += 25;
+                }
+            } 
         }
+
+        if (opcion==2 && cuenta==0)
+        {
+            printf("No se ha ingresado a ningun cliente.\n");
+            return 1;
+        }
+        
         if (opcion == 2)
         {
-            printf("El valor de la cuenta es de: $%d\n", cuenta);
+            printf("El valor de la cuenta de %s es de: $%d\n", cliente, cuenta);
             return 1;
         }
 
     } while (opcion != 3);
+
+    printf("Gracias por utilizar el software :)\n");
     return 0;
 }
